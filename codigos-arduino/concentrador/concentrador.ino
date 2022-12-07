33a8d85069850474233bc1179b3dbcb2676fe641
David Ferrari <david.c.s.ferrari@gmail.com>
2022-12-07 12:16:13 -0300

    Projeto 2 de sismicro

---
+++ codigos-arduino/concentrador/concentrador.ino
@@ -0,0 +1,38 @@
+const int interrupt0 = 2;
+const int num_of_pins = 5;
+const int photo_relay = 12;
+const int button = 11;
+const int thermostat_sensor = 10;
+const int voltage_sensor = 9;
+const int current_sensor = 8;
+uint8_t sensorsArray[num_of_pins] = { current_sensor, voltage_sensor, thermostat_sensor, button, photo_relay};
+char stateArrayValues[num_of_pins];
+volatile int shutdown_button = LOW;
+
+
+void setup() {
+  pinMode(photo_relay, OUTPUT);
+  pinMode(button, OUTPUT);
+  pinMode(thermostat_sensor, OUTPUT);
+  pinMode(voltage_sensor, OUTPUT);
+  pinMode(current_sensor, OUTPUT);
+  pinMode(13, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(interrupt0), send_data, CHANGE);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.readBytes(stateArrayValues, num_of_pins);
+  digitalWrite(current_sensor, stateArrayValues[0]);
+  digitalWrite(voltage_sensor, stateArrayValues[1]);
+  digitalWrite(thermostat_sensor, stateArrayValues[2]);
+  digitalWrite(button, stateArrayValues[3]);
+  digitalWrite(photo_relay, stateArrayValues[4]);
+  digitalWrite(13, shutdown_button);
+  Serial.write(shutdown_button);
+}
+
+void send_data() {
+  shutdown_button = !shutdown_button;
+  digitalWrite(13, shutdown_button);
+}
---
+++ codigos-arduino/controlador/controlador.ino
@@ -0,0 +1,68 @@
+const int interrupt0 = 2; //define interrupt pin to 2
+const int interrupt1 = 3; //define interrupt pin to 3
+const int led = 13;
+const int photo_relay = 12;
+const int button = 11;
+const int thermostat_sensor = 10;
+const int voltage_sensor = 9;
+const int current_sensor = 8;
+const int num_of_pins = 5;
+uint8_t sensorsArray[num_of_pins] = { current_sensor, voltage_sensor, thermostat_sensor, button, photo_relay};
+char stateArrayValues[num_of_pins] = { LOW, LOW, LOW, LOW, LOW };
+const int preLoaderTimeValue = 34285;
+volatile int ledValue = HIGH;
+char receivedData = LOW;
+
+void setup() {
+   pinMode(led, OUTPUT);
+   pinMode(photo_relay, INPUT);
+   pinMode(button, INPUT);
+   pinMode(thermostat_sensor, INPUT);
+   pinMode(voltage_sensor, INPUT);
+   pinMode(current_sensor, INPUT);
+   noInterrupts();                       // disable all interrupts
+   TCCR1A = 0;                           // Timer 1 initialization
+   TCCR1B = 0;                           // Timer 1 initialization
+   TCNT1 = preLoaderTimeValue;           // preload timer
+   TCCR1B |= (1 << CS10)|(1 << CS12);    // 1024 prescaler 
+   TIMSK1 |= (1 << TOIE1);               // enable timer overflow interrupt ISR
+   interrupts();                         // enable all interrupts
+   attachInterrupt(digitalPinToInterrupt(interrupt1), switch_led, CHANGE);
+   attachInterrupt(digitalPinToInterrupt(interrupt0), switch_led, CHANGE);
+   Serial.begin(9600);
+}
+
+void loop() {
+    if(Serial.available() > 0) {
+      receivedData = Serial.read();
+      if(receivedData == LOW)
+        ledValue = HIGH;
+      else if(receivedData == HIGH)
+        ledValue = LOW;
+    }
+    digitalWrite(led, ledValue);
+    for(uint8_t i=0; i<num_of_pins; i++)
+      stateArrayValues[i] = digitalRead(sensorsArray[i]);
+}
+
+void switch_led() {
+   if(any_sensor_is_set())
+      ledValue = LOW;
+   else if(ledValue == LOW && receivedData == LOW)
+      ledValue = HIGH;
+}
+
+bool any_sensor_is_set(){
+  return digitalRead(button) == HIGH || digitalRead(photo_relay) == HIGH
+   || digitalRead(thermostat_sensor) == HIGH || digitalRead(voltage_sensor) == HIGH
+   || digitalRead(current_sensor) == HIGH;
+}
+
+ISR(TIMER1_OVF_vect){
+    TCNT1 = preLoaderTimeValue;
+    send_data();
+}
+
+void send_data() {
+  Serial.write(stateArrayValues, num_of_pins);
+}
